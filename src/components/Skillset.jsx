cda051d8cd2e897db5fba0dd2a829526ef41b579
BenSymons <bsymonsb@hotmail.co.uk>
2021-01-28 15:07:20 +0000

    delete yarn.lock

--- src/components/Skillset.jsx
+++ src/components/Skillset.jsx
@@ -8,31 +8,31 @@ const skillset = () => {
                 <p>Here's a list of the experience I have with programming languages and technologies</p>
                 <div class="skillsbox">
                     <div class="skills">
-                        <h3>Programming languages and frameworks</h3>
+                        <h3>Programming</h3>
                         <ul>
-                            <li>NodeJS</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>SQL</li>
-                            <li>React</li>
-                            <li>GraphQL</li>
-                            <li>Restful APIs</li>
-                            <li>Git</li>
-                            <li>Jest (a testing library)</li>
-                            <li>Python</li>
-                            <li>React native</li>
-                            <li>PHP</li>
+                            <li class="skill">NodeJS</li>
+                            <li class="skill">HTML</li>
+                            <li class="skill">CSS</li>
+                            <li class="skill">SQL</li>
+                            <li class="skill">React</li>
+                            <li class="skill">GraphQL</li>
+                            <li class="skill">Restful APIs</li>
+                            <li class="skill">Git</li>
+                            <li class="skill">Jest (a testing library)</li>
+                            <li class="skill">Python</li>
+                            <li class="skill">React native</li>
+                            <li class="skill">PHP</li>
                         </ul>
                     </div>
                     <div class="skills">
                         <h3>Other skills</h3>
                         <ul>
-                            <li>Microsoft Excel</li>
-                            <li>Microsoft Word</li>
-                            <li>Microsoft Powerpoint</li>
-                            <li>Basic 3D modelling using Blender</li>
-                            <li>Video editing using Blender</li>
-                            <li>Image editing using Gimp</li>
+                            <li class="skill">Microsoft Excel</li>
+                            <li class="skill">Microsoft Word</li>
+                            <li class="skill">Microsoft Powerpoint</li>
+                            <li class="skill">Basic 3D modelling using Blender</li>
+                            <li class="skill">Video editing using Blender</li>
+                            <li class="skill">Image editing using Gimp</li>
                         </ul>
                     </div>
                 </div>
